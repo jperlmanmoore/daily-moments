6f3e7ca9d69277714ee6c3f80efd12bab36ae477
Jennifer <jperlmanmoore@gmail.com>
2020-09-18 13:44:55 -0400

    firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,18 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import * as firebase from 'firebase/app';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBTDIX0-wEhIAm_ls-N6JrXLTwgzVIublQ",
-    authDomain: "daily-moments-b6549.firebaseapp.com",
-    databaseURL: "https://daily-moments-b6549.firebaseio.com",
-    projectId: "daily-moments-b6549",
-    storageBucket: "daily-moments-b6549.appspot.com",
-    messagingSenderId: "563609522901",
-    appId: "1:563609522901:web:e010f001049c20f358885c",
-    measurementId: "G-Y9NE2525WC"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
+
+  const app = firebase.initializeApp(firebaseConfig);
+
+ 
