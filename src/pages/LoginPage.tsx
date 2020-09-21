a0b3b0066a0c8daafe47ebb3d5a42b0b264ce63e
Jennifer <jperlmanmoore@gmail.com>
2020-09-21 12:15:01 -0400

    email authentication with google

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,11 +5,18 @@ import {
     IonTitle,
     IonToolbar,
     IonButton,
+    IonList,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonText,
+    IonLoading,
 } from '@ionic/react';
 import React from 'react';
 import { Redirect } from 'react-router';
 import { useAuth } from '../auth';
 import { auth } from '../firebase';
+import {useState} from 'react';
 
 interface Props {
     onLogin: () => void;
@@ -18,10 +25,23 @@ interface Props {
   const LoginPage: React.FC<Props> = ( { onLogin} ) => {
     // custom hook useAuth() - this component doesn't worry about React Context 
     const { loggedIn} = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [status, setStatus] = useState({loading: false, error: false});
+  
 
     const handleLogin = async () => {
-      const credential = await auth.signInWithEmailAndPassword('test@test.test', 'test123');
+      console.log({email, password});
+      try {
+      const credential = await auth.signInWithEmailAndPassword(email, password);
+      setStatus({loading: false, error: false})
       console.log('credential:', credential);
+      onLogin();
+      } catch (error) {
+        setStatus({loading: false, error: true});
+        console.log('error:', error);
+      }
+      
     };
       if (loggedIn) {
        return <Redirect to="/my/entries" />;
@@ -34,7 +54,28 @@ interface Props {
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
+          <IonList>
+            <IonItem>
+              <IonLabel position="stacked">Email</IonLabel>
+              <IonInput type="email" value={email}
+              onIonChange={(event => setEmail(event.detail.value))}
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Password</IonLabel>
+              <IonInput type="password" value={password}
+              onIonChange={(event => setPassword(event.detail.value))}
+              />
+            </IonItem>
+          </IonList>
+          {/* conditionally render - if err, then show this */}
+          {status.error &&
+          <IonText color="danger">invalid credential</IonText>
+        }
         <IonButton expand="block" onClick={handleLogin}>LOGIN</IonButton> 
+        {
+        <IonLoading isOpen={status.loading=false} />
+  }
         </IonContent>
       </IonPage>
     );
